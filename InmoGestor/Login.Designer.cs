883046b672430243e7d83f23fb130fa684da5302
Ivan-Gauto <ivangauto602160@gmail.com>
2025-08-28 00:01:36 -0300

    Se creo vista de Login

---
+++ InmoGestor/Login.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace InmoGestor
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(15, 30, 45);
+            panel1.Controls.Add(label3);
+            panel1.ForeColor = Color.White;
+            panel1.Location = new Point(64, 220);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(425, 360);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(161, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(245, 54);
+            label1.TabIndex = 1;
+            label1.Text = "InmoGestor";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ActiveCaption;
+            label2.Location = new Point(148, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(267, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Sistema de gestion de alquileres";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(156, 31);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 0;
+            label3.Text = "label3";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(10, 20, 35);
+            ClientSize = new Size(548, 626);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Login";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
